8b42ed9532394d96c18180f8c65cbfd83e6211dc
FRANCESCO FIORELLA s303922 <s303922@studenti.polito.it>
2022-11-07 22:22:28 +0100

    Added user api

--- client/src/API.js
+++ client/src/API.js
@@ -1,5 +1,74 @@
-const SERVER_URL = 'http://localhost:3001';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, sendEmailVerification  } from "firebase/auth";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCxwXhRO1caJvjh5uRHOOQ7EMJtaAIZG2E",
+  authDomain: "hiketracker-585f6.firebaseapp.com",
+  projectId: "hiketracker-585f6",
+  storageBucket: "hiketracker-585f6.appspot.com",
+  messagingSenderId: "773343738623",
+  appId: "1:773343738623:web:a33524701d8bee893e2aca",
+  measurementId: "G-09SB328BFS"
+};
 
-const API = { };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+const signUp = (email, password) => {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // Send verification emeil
+        sendVerificationEmail();
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
+}
+
+const logIn = (email, password) => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
+}
+
+const logOut = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+        // Sign-out successful.
+    }).catch((error) => {
+        // An error happened.
+    });
+}
+
+const sendVerificationEmail = () => {
+    const auth = getAuth();
+    sendEmailVerification(auth.currentUser)
+    .then(() => {
+        // Email verification sent!
+        // ...
+    });
+}
+
+const API = { signUp, logIn, logOut };
 export default API;
