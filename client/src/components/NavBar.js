edd7ddaa6a6401035ebada536d27795dc66f97f4
mistru97 <luca.mistruzzi@gmail.com>
2022-11-09 00:48:05 +0100

    fix: render for small devices

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import { Navbar, DropdownButton, Dropdown, Button, Container } from 'react-bootstrap';
+import { Navbar, DropdownButton, Dropdown, Button, Container, Row, Col } from 'react-bootstrap';
 import { NavLink, useLocation } from 'react-router-dom';
 import { FaRegUserCircle, FaHiking } from 'react-icons/fa'
 import AuthenticationContext from './AuthenticationContext';
@@ -12,30 +12,38 @@ const NavBar = (props) => {
         <AuthenticationContext.Consumer>
             {(authObject) => (
                 <>
-                    <Navbar expand="sm">
-                        <Container>
-                            <Navbar.Brand>
-                                <FaHiking className='react-icon' />
-                                Hike Tracker
-                            </Navbar.Brand>
-                            <Navbar.Toggle aria-controls="nav-toggle" />
-                            <Navbar.Collapse className='justify-content-end' id="nav-toggle">
-                                {authObject.authUser &&
-                                    <>
-                                        <DropdownButton className='d-flex align-items-center' title={<><FaRegUserCircle className='react-icon align-self-center' />{'   '}{authObject.authUser.name.toUpperCase()}</>} variant='outline-dark' menuAlign={{ sm: 'right' }}>
-                                            <Dropdown.Item onClick={() => authObject.onLogout()}>Logout</Dropdown.Item>
-                                        </DropdownButton>
-                                    </>
-                                }
-                                {!authObject.authUser &&
-                                    <>
-                                        <NavLink to='/login'><Button variant='link-dark'>Sign In</Button></NavLink>
-                                        <NavLink to='/signup'><Button variant='outline-dark'>Sign Up</Button></NavLink>
-                                    </>
-                                }
-                            </Navbar.Collapse>
-                        </Container>
-                    </Navbar>
+                    <Container fluid>
+                        <Row>
+                            <Col lg={2} />
+                            <Col>
+                            <Navbar expand="sm">
+                                <Navbar.Brand>
+                                    <FaHiking className='react-icon' />
+                                    Hike Tracker
+                                </Navbar.Brand>
+                                <Navbar.Toggle aria-controls="nav-toggle" />
+                                <Navbar.Collapse className='justify-content-end' id="nav-toggle">
+                                    {authObject.authUser &&
+                                        <>
+                                            <DropdownButton className='d-flex align-items-center' title={<><FaRegUserCircle className='react-icon align-self-center'/>{'   '}{authObject.authUser.name.toUpperCase()}</>} variant='outline-dark' align={{ sm: 'end' }} menuVariant='dark'>
+                                                <Dropdown.Item ><NavLink className='profile-link' to={`/${authObject.authUser.name.toLowerCase()}`} variant='dark'>Your profile</NavLink></Dropdown.Item>
+                                                <Dropdown.Divider />
+                                                <Dropdown.Item onClick={() => authObject.onLogout()}>Sign out</Dropdown.Item>
+                                            </DropdownButton>
+                                        </>
+                                    }
+                                    {!authObject.authUser &&
+                                        <>
+                                            <NavLink to='/login'><Button variant='link-dark'>Sign In</Button></NavLink>
+                                            <NavLink to='/signup'><Button variant='outline-dark'>Sign Up</Button></NavLink>
+                                        </>
+                                    }
+                                </Navbar.Collapse>
+                            </Navbar>
+                            </Col>
+                            <Col lg={2}/>
+                        </Row>
+                    </Container>
                 </>
             )}
         </AuthenticationContext.Consumer>
