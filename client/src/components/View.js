c70f22c3c4ba4160d4b52e23878b4ef6a2c138b4
mistru97 <luca.mistruzzi@gmail.com>
2022-11-09 00:47:28 +0100

    fix: removed components

--- client/src/components/View.js
+++ client/src/components/View.js
@@ -5,12 +5,8 @@ import { Link, Outlet } from 'react-router-dom';
 function DefaultRoute() {
     return (
         <>
-            <Row>
-                <Col>
-                    <h1>Nothing here...</h1>
-                    <p>This is not the route you are looking for!</p>
-                </Col>
-            </Row>
+            <h1>Nothing here...</h1>
+            <p>This is not the route you are looking for!</p>
         </>
     );
 }
