a2586563527fe2aab7e8e7235bfacff54c6df54e
mistru97 <luca.mistruzzi@gmail.com>
2022-11-09 17:55:13 +0100

    fix: comment login/out to prevent contact server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { AppLayout, DefaultRoute } from './components/View';
+import { BrowserHikes, DefaultRoute } from './components/View';
 import { useEffect, useState } from 'react';
 import API from './API.js'
 import NavBar from './components/NavBar';
@@ -22,22 +22,22 @@ function App() {
   //login and logut functions
   const login = async (email, password) => {
     try {
-      const user = await API.logIn(email, password);
-      console.log(user);
+      // const user = await API.logIn(email, password);
+      // console.log(user);
       setAuthUser({name: "Gianmarco"});
     } catch (err) {
       setAuthErr(err);
     }
   }
   const logout = async () => {
-    await API.logOut();
+    // await API.logOut();
     setAuthUser(undefined);
     setAuthErr(undefined);
   }
 
   const signup = async (email, password, firstName, lastName) => {
     try {
-      const user = await API.signUp(email, password, firstName, lastName);
+      // const user = await API.signUp(email, password, firstName, lastName);
     } catch (err) {
       setMessage({ msg: err, type: 'danger' });
     }
@@ -57,14 +57,15 @@ function App() {
       <AuthenticationContext.Provider value={authObject}>
         <BrowserRouter>
           <NavBar />
-          <Container fluid>
+          <Container fluid className='PageContainer'>
+            <Row/>
             <Row>
               <Col lg={2}/>
               <Col>
                 <Routes>
 
                   <Route path='/' element={<Navigate to='/home'/>}/>
-                  <Route path='/home' element={<AppLayout onLogOut={logout} loggedIn={login}/>}></Route>
+                  <Route path='/home' element={<BrowserHikes onLogOut={logout} loggedIn={login}/>}></Route>
                   <Route path='/login' element={authUser ? <Navigate to='/home'/> : <LoginForm login={login} />}/>
                   <Route path='/signup' element={<SigninForm signup={signup}/>}></Route>
 
