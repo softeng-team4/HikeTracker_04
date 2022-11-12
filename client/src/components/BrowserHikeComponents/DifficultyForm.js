25f40fc054b79a19b415563e97f9248aff5f3776
mistru97 <luca.mistruzzi@gmail.com>
2022-11-12 14:42:06 +0100

    fix: difficulty level name, grid layout

--- client/src/components/BrowserHikeComponents/DifficultyForm.js
+++ client/src/components/BrowserHikeComponents/DifficultyForm.js
@@ -7,30 +7,34 @@ const DifficultyForm = (props) => {
 
 
     //list for display difficulties on component
-    const difficultyList = ['easy', 'medium', 'hard'];
+    const difficultyList = ['Tourist', 'Hiker', 'Pro Hiker'];
     // state to highlight selected button
     const [selectedButton, setSelectedButton] = useState(undefined)
 
 
     const handleButtonClick = (key) => {
-        if(selectedButton === key) {
+        if (selectedButton === key) {
             setSelectedButton(undefined);
             key = 'None';
         } else
             setSelectedButton(key);
+        if (key === 'Pro Hiker')
+            key = 'Professional Hiker'
         props.handleDifficultySubmit(key);
     };
 
 
     return (
-        <Form className='d-flex justify-content-between'>
-            <Form.Group className='col-md-12 p-2'>
-                <Form.Label htmlFor='CountrySelection'>Select a difficulty&nbsp;</Form.Label>
-                <ButtonGroup>
-                    {difficultyList.map((d) =>
-                        <Button key={d} active={d === selectedButton ? true : false} variant='light' onClick={(ev) => (handleButtonClick(d))}>{d}</Button>
-                    )}
-                </ButtonGroup>
+        <Form className='row col-md d-flex justify-content-between'>
+            <Form.Group className='row col-md p-2'>
+                <Form.Label className='' htmlFor='DifficultySelection'>Select a difficulty&nbsp;</Form.Label>
+                <Form.Text>
+                    <ButtonGroup className='DifficultySelection '>
+                        {difficultyList.map((d) =>
+                            <Button key={d} active={d === selectedButton ? true : false} variant='light' onClick={(ev) => (handleButtonClick(d))}>{d}</Button>
+                        )}
+                    </ButtonGroup>
+                </Form.Text>
             </Form.Group>
         </Form>
     );
