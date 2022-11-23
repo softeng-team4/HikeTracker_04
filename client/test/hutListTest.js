dbec466cee5f4c985bc8a8a6fb9f2c491bae14f6
Federico Siviero s301196 <s301196@studenti.polito.it>
2022-11-23 21:32:53 +0100

    Improved the setup for testing hut browsing api

--- client/test/hutListTest.js
+++ client/test/hutListTest.js
@@ -15,10 +15,16 @@ const api = require('../src/API');
 //import { getFirestore, doc, query, collection, getDocs, deleteDoc, documentId, getDoc} from "firebase/firestore";
 //import {addNewHike} from "../src/API"
 
-const testHikes = firestore.collection(api.db,"hike-test")
+const testHuts = firestore.collection(api.db,"hut-test")
 
 describe('test the searching for huts by a hiker',() =>{
 
+    const hut1 = {
+        name: "Rifugio", bedsNumber:10, description:"a nice place to rest", openingHour: 8, openingMinute: 30, closingHour: 21, closingMinute: 0, costPerNight: 30, position:[21.2,11.3]}
+    const hut2 = {
+        name: "Capanna remota", bedsNumber:15, description:"a nice place to relax", openingHour: 8, openingMinute: 30, closingHour: 21, closingMinute: 0, costPerNight: 70, position:[30.2,11.1]}
+        const hut3 = {
+            name: "Ristoro montano", bedsNumber:30, description:"you'll never want to leave", openingHour: 7, openingMinute: 0, closingHour: 22, closingMinute: 30, costPerNight: 25, position:[25.8,31.4]}
     before(async () =>{
 
         const hutQuery = firestore.query(testHuts);
@@ -27,18 +33,31 @@ describe('test the searching for huts by a hiker',() =>{
             await firestore.deleteDoc(firestore.doc(db,"hut-test",doc.id))
         })
 
-        await firestore.setDoc(firestore.doc(testHuts, "1"), {
-            name: "Rifugio", city: "Turin", country: "Italy",
-            region: "Piemonte", coordinates:"", altitude:2000, nBeds:0, description:"", });
+        await firestore.setDoc(firestore.doc(testHuts, "1"), hut1);
+        await firestore.setDoc(firestore.doc(testHuts, "2"), hut2);
+        await firestore.setDoc(firestore.doc(testHuts, "3"), hut3);
 
     })
+    testHutList([hut1,hut2,hut3])
 })
 
-function testHutList(filter, n){
-    it("Viewing a list of huts with some filters ",function(done){
+function testHutList(huts){
+    it("Viewing a list of huts",function(done){
         api.hutsList(filter,"hut-test")
         .then((res)=>{
             res.length.should.equal(n)
+            for(let i = 0; i < res.length; i++){
+                res[i].name.should.equal(huts[i].name)
+                res[i].bedsNumber.should.equal(huts[i].bedsNumber)
+                res[i].description.should.equal(huts[i].description)
+                res[i].openingHour.should.equal(huts[i].openingHour)
+                res[i].openingMinute.should.equal(huts[i].openingMinute)
+                res[i].closingHour.should.equal(huts[i].closingMinute)
+                res[i].closingMinute.should.equal(huts[i].closingMinute)
+                res[i].costPerNight.should.equal(huts[i].costPerNight)
+                res[i].position[0].should.equal(huts[i].position[0])
+                res[i].position[1].should.equal(huts[i].position[1])
+            }
             })
         .then(() => done(), done)
         .catch((error) => {
