0ce9c67f7fc9bb503d1a497f60e7265024990d73
gianmarcobll <80529690+gianmarcobll@users.noreply.github.com>
2022-11-23 20:05:16 +0100

    Update API.js

--- client/src/API.js
+++ client/src/API.js
@@ -180,17 +180,17 @@ function distance (lat1, lon1, lat2, lon2){
 		return 0;
 	}
 	else {
-        const R = 6371e3;
-        const p1 = lat1 * Math.PI/180;
-        const p2 = lat2 * Math.PI/180;
-        const deltaP = p2 - p1;
-        const deltaLon = lon2 - lon1;
-        const deltaLambda = (deltaLon * Math.PI) / 180;
-        const a = Math.sin(deltaP/2) * Math.sin(deltaP/2) +
-                  Math.cos(p1) * Math.cos(p2) *
-                  Math.sin(deltaLambda/2) * Math.sin(deltaLambda/2);
-        const d = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)) * R;
-        return d;
+		const R = 6371e3;
+		const p1 = lat1 * Math.PI/180;
+		const p2 = lat2 * Math.PI/180;
+		const deltaP = p2 - p1;
+		const deltaLon = lon2 - lon1;
+		const deltaLambda = (deltaLon * Math.PI) / 180;
+		const a = Math.sin(deltaP/2) * Math.sin(deltaP/2) +
+			  Math.cos(p1) * Math.cos(p2) *
+			  Math.sin(deltaLambda/2) * Math.sin(deltaLambda/2);
+		const d = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)) * R;
+		return d;
 	}
 }
 
