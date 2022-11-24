5f79388913f32257e50b134b1096df82efaba88d
mistru97 <luca.mistruzzi@gmail.com>
2022-11-24 13:02:41 +0100

    fix: button show more info fixed layout

--- client/src/components/BrowserHikeComponents/HikeTable.js
+++ client/src/components/BrowserHikeComponents/HikeTable.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col, Container, Card, Button } from 'react-bootstrap';
-import { useEffect, useState } from 'react';
+import { Row, Col, Container, Card, Button, Overlay, Tooltip, OverlayTrigger } from 'react-bootstrap';
+import { useEffect, useRef, useState } from 'react';
 import Spacer from './Spacer';
 import FilterForm from './FilterForm';
 import AuthenticationContext from '../AuthenticationContext';
@@ -56,20 +56,34 @@ const HikeTable = () => {
                         {subHikeList.map((hike, idx) =>
                             <div key={`div_${idx}`}>
                                 <Card key={`card_${idx}`}>
-                                    <Card.Header className='row d-flex justify-content-start' key={`card_header_${idx}`}>
-                                        <Col md={3}><b>Title:</b>&nbsp;{hike.title}</Col>
-                                        <Col md={3}><b>Location:</b>&nbsp;{hike.country},&nbsp;{hike.region},&nbsp;{hike.city}</Col>
-                                        {authObject.authUser ? <Button key={hike.id} variant='success' size='sm' onClick={(ev) => handleShowInfo(ev)}>Show more info</Button> : null}
+                                    <Card.Header key={`card_header_${idx}`}>
+                                        <Row md={10}>
+                                            <Col md={5}><b>Title:</b>&nbsp;{hike.title}</Col>
+                                            <Col md={5}><b>Location:</b>&nbsp;{hike.country},&nbsp;{hike.region},&nbsp;{hike.city}</Col>
+                                            <Col className='d-flex justify-content-md-end'>
+                                                <OverlayTrigger overlay={!authObject.authUser ? <Tooltip id="tooltip-disabled">Sign up to see more info about the hike</Tooltip> : <></>}>
+                                                    <Button
+                                                        key={hike.id}
+                                                        variant='success'
+                                                        size='sm'
+                                                        onClick={authObject.authUser ? (ev) => handleShowInfo(ev) : null}>
+                                                        Show more info
+                                                    </Button>
+                                                </OverlayTrigger>
+                                            </Col>
+                                        </Row>
                                     </Card.Header>
-                                    <Card.Body className='row d-flex justify-content-start' key={`card_body_${idx}`}>
+                                    <Card.Body className='d-flex justify-content-start' key={`card_body_${idx}`}>
                                         <Col><b>Description:</b>&nbsp;<Col className='hike-desc'>{hike.description}</Col></Col>
                                     </Card.Body>
-                                    <Card.Footer className='row d-flex justify-content-between' key={`card_footer_${idx}`}>
-                                        <Col md={3} key={`hike_diff_${idx}`}><b>Difficulty:</b>&nbsp;{hike.difficulty}</Col>
-                                        <Col md={3} key={`hike_len_${idx}`}><b>Length:</b>&nbsp;{hike.length}&nbsp;km</Col>
-                                        <Col md={3} key={`hike_asc_${idx}`}><b>Ascent:</b>&nbsp;{hike.ascent}&nbsp;m</Col>
-                                        <Col md={3} key={`hike_time_${idx}`}><b>Estimated Time:</b>&nbsp;{hike.expectedTime}&nbsp;min</Col>
-                                        {/* {authObject.authUser && authObject.authUser.role.toLowerCase() === 'local guide' ? //TODO modify button : null} */}
+                                    <Card.Footer key={`card_footer_${idx}`}>
+                                        <Row md={12}>
+                                            <Col md key={`hike_diff_${idx}`}><b>Difficulty:</b>&nbsp;{hike.difficulty}</Col>
+                                            <Col md key={`hike_len_${idx}`}><b>Length:</b>&nbsp;{hike.length}&nbsp;km</Col>
+                                            <Col md key={`hike_asc_${idx}`}><b>Ascent:</b>&nbsp;{hike.ascent}&nbsp;m</Col>
+                                            <Col md key={`hike_time_${idx}`}><b>Estimated Time:</b>&nbsp;{hike.expectedTime}&nbsp;min</Col>
+                                            {/* {authObject.authUser && authObject.authUser.role.toLowerCase() === 'local guide' ? //TODO modify button : null} */}
+                                        </Row>
                                     </Card.Footer>
                                 </Card>
                                 <Spacer height='1rem' key={`card_spacer_${idx}`} />
@@ -78,7 +92,7 @@ const HikeTable = () => {
                         {hikeList.length === 0 ? <Container className='emty-hikeList'><Spacer height='2rem' /><Card><h5>There are no hikes for the selected filters!</h5></Card><Spacer height='2rem' /></Container> : null}
                         <HikePageHandler index={index} pageNum={computeIndex()} handlePageChange={handlePageChange} />
                     </Container>
-                    <AdditionalHikeInfoModal hikeId={hikeId} show={showInfoModal} onHide={() => setShowInfoModal(false)}/>
+                    <AdditionalHikeInfoModal hikeId={hikeId} show={showInfoModal} onHide={() => setShowInfoModal(false)} />
                 </>
             )}
         </AuthenticationContext.Consumer>
