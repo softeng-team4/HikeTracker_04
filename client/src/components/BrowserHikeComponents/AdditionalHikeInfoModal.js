5f895674314580b300754e0ef6d94eea9dad86ea
mistru97 <luca.mistruzzi@gmail.com>
2022-11-25 15:56:47 +0100

    add: basic info and title to the modal

--- client/src/components/BrowserHikeComponents/AdditionalHikeInfoModal.js
+++ client/src/components/BrowserHikeComponents/AdditionalHikeInfoModal.js
@@ -11,6 +11,7 @@ const AdditionalHikeInfoModal = (props) => {
         <Modal {...props} size='lg' aria-labelledby="contained-modal-title-vcenter" centered>
             <Modal.Header className='m-0'>
                 <Col>
+                    <Modal.Title>Hike:&nbsp;{props.hike.title}</Modal.Title>
                     <MapContainer center={[45.46427, 9.18951]} zoom={13} zoomControl={false}>
                         <TileLayer
                             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -20,7 +21,11 @@ const AdditionalHikeInfoModal = (props) => {
                 </Col>
             </Modal.Header>
             <Modal.Body>
-                {'ADD INFO HERE'}
+                <Col><strong>Description:</strong>&nbsp;{props.hike.description}</Col>
+                <Col><strong>Difficulty:</strong>&nbsp;{props.hike.difficulty}</Col>
+                <Col><strong>Length:</strong>&nbsp;{props.hike.length}&nbsp;km</Col>
+                <Col><strong>Ascent:</strong>&nbsp;{props.hike.ascent}&nbsp;m</Col>
+                <Col><strong>Estimated Time:</strong>&nbsp;{props.hike.expectedTime}&nbsp;min</Col>
             </Modal.Body>
             <Modal.Footer>
                 <Button size='sm' onClick={props.onHide}>Close</Button>
