ed418cf2073e85af97eb6ec58e126a8a049fe0fc
FRANCESCO FIORELLA s303922 <s303922@studenti.polito.it>
2022-12-08 11:42:05 +0100

    Added user class

---
+++ client/src/model/User.js
@@ -0,0 +1,10 @@
+export default function User(email, firstName, lastName, role, reqRole, reqStatus, respDate, hut) {
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.role = role;
+    this.reqRole = reqRole;
+    this.reqStatus = reqStatus;
+    this.respDate = respDate;
+    this.hut = hut;
+}
