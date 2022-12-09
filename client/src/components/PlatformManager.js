fa8315d2d693ccb7c76adb505640ea95fdeee06c
Alessandro Ganino s304890 <73252380+aleganino@users.noreply.github.com>
2022-12-09 17:35:22 +0100

    Added role filter to page

--- client/src/components/PlatformManager.js
+++ client/src/components/PlatformManager.js
@@ -1,26 +1,47 @@
 import { useEffect, useState } from 'react';
-import { Button, ButtonGroup, Card, Col, ListGroup, Modal, Row } from 'react-bootstrap';
+import { Button, ButtonGroup, Card, Col, ListGroup, Modal, Row, Container, Form } from 'react-bootstrap';
 import { AiOutlineCheck, AiOutlineStop } from 'react-icons/ai'
 import API from '../API';
 import AuthenticationContext from './AuthenticationContext';
 import Spacer from './BrowserHikeComponents/Spacer';
 
 function PlatformManager(props) {
+    const roleList = ["Local guide", "Hut Worker"]
+    const [requestedRole, setRequestedRole] = useState('')
     const [requestingUsers, setRequestingUsers] = useState([]);
     const [reload, setReload] = useState(false);
     const [showAccepted, setShowAccepted] = useState(false);
-    const [showRejected, setShowRejected] = useState(false);
+    const [showRejected, setShowRejected] = useState(false);    
+    const [selectedButton, setSelectedButton] = useState(undefined)
+
     useEffect(() => {
 
         API.getRequestingUsers().then(r => setRequestingUsers(r))
 
     }, [requestingUsers.length, reload])
-    console.log(requestingUsers)
 
     const handleRequest = async (user, status) => {
         await API.handleRoleRequest(user, status);
         status === 'accepted' ? setShowAccepted(true) : setShowRejected(true)
         setReload(!reload);
+    }
+
+    
+    const handleButtonClick = (key) => {
+        if (selectedButton === key) {
+            setSelectedButton('');
+            key = '';
+        } else
+            setSelectedButton(key);
+        setRequestedRole(key);
+    };
+
+    const filterRole = (elem) => {
+        if (requestedRole==='')
+            return true
+        else if (elem.reqRole.toLowerCase()===requestedRole.toLowerCase())
+            return true
+        else return false
     }
 
     return (
@@ -55,40 +76,59 @@ function PlatformManager(props) {
                         </Modal.Footer>
                     </Modal>
 
-                    {requestingUsers.map((r, i) => <div key={`div_${i}`} >
-                        <Card key={`card_${i}`}>
-                            <Card.Header key={`card_header_${i}`}>
-                                <Row md={10}>
-                                    <Col md={5}><b>User:</b>&nbsp;{r.firstName}&nbsp;{r.lastName}</Col>
-                                    <Col md={5}><b>Requested Role:</b>&nbsp;{r.reqRole}</Col>
-                                    <Col className='d-flex justify-content-md-end'>
-                                        <ButtonGroup size='sm'>
-                                            <Button variant='success' onClick={() => { handleRequest(r, 'accepted') }}>
-                                                <AiOutlineCheck />
-                                            </Button>
-                                            <Button variant='danger' onClick={() => { handleRequest(r, 'rejected') }}>
-                                                <AiOutlineStop />
-                                            </Button>
-                                        </ButtonGroup>
-                                    </Col>
-                                </Row>
-                            </Card.Header>
-                            <Card.Body className='d-flex justify-content-start' key={`card_body_${i}`}>
+                    <Form className='row col-md d-flex justify-content-between'>
+                        <Form.Group as={Row} className='row col-md p-2'>
+                            <Col sm={2}>
+                            <Form.Label htmlFor='RoleSelection'>Filter requests by role: &nbsp;</Form.Label>
+                            </Col>
+                            <Col>
+                            <Form.Text>
+                                <ButtonGroup className='RoleSelection'>
+                                    {roleList.map((d) =>
+                                        <Button key={d} active={d === selectedButton ? true : false} variant='light' onClick={(ev) => (handleButtonClick(d))}>{d}</Button>
+                                    )}
+                                </ButtonGroup>
+                            </Form.Text>
+                            </Col>
+                        </Form.Group>
+                    </Form>
 
-                                <Row md={12}>
-                                    <Col md key={`user_email_${i}`}><b>Email:</b>&nbsp;{r.email}</Col>
-                                </Row>
-                            </Card.Body>
-                            <Card.Footer key={`card_footer_${i}`}>
-                                <Row md={12}>
-                                    <Col md key={`user_role_${i}`}><b>Role:</b>&nbsp;{r.role}</Col>
-                                </Row>
+                    {requestingUsers.filter(filterRole).map((r, i) =>
+                        <div key={`div_${i}`} >
+                            <Card key={`card_${i}`}>
+                                <Card.Header key={`card_header_${i}`}>
+                                    <Row md={10}>
+                                        <Col md={5}><b>User:</b>&nbsp;{r.firstName}&nbsp;{r.lastName}</Col>
+                                        <Col md={5}><b>Requested Role:</b>&nbsp;{r.reqRole}</Col>
+                                        <Col className='d-flex justify-content-md-end'>
+                                            <ButtonGroup size='sm'>
+                                                <Button variant='success' onClick={() => { handleRequest(r, 'accepted') }}>
+                                                    <AiOutlineCheck />
+                                                </Button>
+                                                <Button variant='danger' onClick={() => { handleRequest(r, 'rejected') }}>
+                                                    <AiOutlineStop />
+                                                </Button>
+                                            </ButtonGroup>
+                                        </Col>
+                                    </Row>
+                                </Card.Header>
+                                <Card.Body className='d-flex justify-content-start' key={`card_body_${i}`}>
 
-                            </Card.Footer>
-                        </Card>
-                        <Spacer height='1rem' key={`card_spacer_${i}`} />
-                    </div>
+                                    <Row md={12}>
+                                        <Col md key={`user_email_${i}`}><b>Email:</b>&nbsp;{r.email}</Col>
+                                    </Row>
+                                </Card.Body>
+                                <Card.Footer key={`card_footer_${i}`}>
+                                    <Row md={12}>
+                                        <Col md key={`user_role_${i}`}><b>Role:</b>&nbsp;{r.role}</Col>
+                                    </Row>
+
+                                </Card.Footer>
+                            </Card>
+                            <Spacer height='1rem' key={`card_spacer_${i}`} />
+                        </div>
                     )}
+                    {requestingUsers.filter(filterRole).length === 0 && <Container className='emty-hikeList'><Spacer height='2rem' /><Card><h5>No request found!</h5></Card><Spacer height='2rem' /></Container>}
 
                 </>
             )}
