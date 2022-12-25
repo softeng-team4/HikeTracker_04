99cb6656fdae22a909ae9fcd417906bd3803aa01
Alessandro Ganino s304890 <73252380+aleganino@users.noreply.github.com>
2022-12-25 18:32:49 +0100

    Created HikeCard component & added to active hike page

--- client/src/components/ActiveHikePage.js
+++ client/src/components/ActiveHikePage.js
@@ -2,9 +2,34 @@ import AuthenticationContext from './AuthenticationContext';
 import { Container, Card, Col, Row } from 'react-bootstrap';
 import Spacer from './BrowserHikeComponents/Spacer';
 import API from '../API'
+import HikeCard from './BrowserHikeComponents/HikeCard';
 
 function ActiveHikePage(props) {
-    const hike = props.hike
+    const hikeId = props.hikeId
+    //const hike = API.getHikeById(hikeId)
+    const hike = {
+        ascent:"764.57",
+        author:"aleganino@gmail.com",
+        city:"Stroppiana",
+        country:"Italy",
+        description: "Partenza da Le Combe",
+        difficulty:"Tourist",
+        endPoint: {
+            altitude: 1648.35,
+            latitude: 45.1797556,
+            longitude: 7.164966
+        },
+        expectedTime: "360",
+        length: "9875.04",
+        region: "Piedmont",
+        startPoint:{
+            altitude: 1641.59,
+            latitude: 45.179357,
+            longitude: 7.1644689
+        },
+        title: hikeId==='1ougz4pxvyWg7AZOKnIA',
+        referencePoint: '[{"lat":45.179357,"lng":7.1644689},{"lat":45.1793465,"lng":7.1644876},{"lat":45.1793246,"lng":7.1645212},{"lat":45.1793012,"lng":7.1645587},{"lat":45.1792856,"lng":7.1645948},{"lat":45.1792792,"lng":7.1646235}]',
+    }
     return (
         <AuthenticationContext.Consumer>
             {(authObject) => (
@@ -12,29 +37,7 @@ function ActiveHikePage(props) {
                     <Container fluid style={{ marginBottom: 20 }}>
                         <Spacer height='2rem' />
                         <h2>Active Hike</h2>
-                        <Card key={`card`}>
-                                    <Card.Header key={`card_header`}>
-                                        <Row>
-                                            <Col md={8}>
-                                                <Row>
-                                                    <Col lg={6}><b>Title:</b>&nbsp;{hike.title}</Col>
-                                                    <Col lg={6}><b>Location:</b>&nbsp;{hike.country},&nbsp;{hike.region},&nbsp;{hike.city}</Col>
-                                                </Row>
-                                            </Col>
-                                        </Row>
-                                    </Card.Header>
-                                    <Card.Body className='d-flex justify-content-start' key={`card_body`}>
-                                        <Col><b>Description:</b>&nbsp;<Col className='hike-desc'>{hike.description}</Col></Col>
-                                    </Card.Body>
-                                    <Card.Footer key={`card_footer`}>
-                                        <Row md={12}>
-                                            <Col md={6} lg={3} key={`hike_diff`}><b>Difficulty:</b>&nbsp;{hike.difficulty}</Col>
-                                            <Col md={6} lg={3} key={`hike_len`}><b>Length:</b>&nbsp;{(parseFloat(hike.length) / 1000.).toFixed(1)}&nbsp;km</Col>
-                                            <Col md={6} lg={3} key={`hike_asc`}><b>Ascent:</b>&nbsp;{parseInt(hike.ascent)}&nbsp;m</Col>
-                                            <Col md={6} lg={3} key={`hike_time`}><b>Estimated Time:</b>&nbsp;{hike.expectedTime}&nbsp;min</Col>
-                                        </Row>
-                                    </Card.Footer>
-                                </Card>
+                        <HikeCard hike={hike}/>
                     </Container>
                 </>
             )}
---
+++ client/src/components/BrowserHikeComponents/HikeCard.js
@@ -0,0 +1,86 @@
+import AuthenticationContext from "../AuthenticationContext";
+import { Row, Col, Container, Card, ButtonGroup, Button, Tooltip, OverlayTrigger, Spinner, Modal } from 'react-bootstrap';
+import AdditionalHikeInfoModal from './AdditionalHikeInfoModal';
+import ConfirmModal from '../ModifyHikeComponents/ConfirmModal';
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+const HikeCard = (props) => {
+    // state to display modal with additional hike info
+    const [showInfoModal, setShowInfoModal] = useState(false);
+    const [hike, setHike] = useState(props.hike);
+    const [showConfirm, setShowConfirm] = useState(false);
+    const navigate = useNavigate()
+    // function to display additional hike info modal
+    const handleShowInfo = (event) => {
+        event.preventDefault();
+        const id = event.target.id;
+        setHike(hike);
+        setShowInfoModal(true);
+    }
+
+    const startHike = async (hikeId) => {
+        //await API.startHike(hikeId);
+        navigate(`/active/${hikeId}`)
+    }
+
+
+    return (
+        <AuthenticationContext.Consumer>
+            {(authObject) => (
+                <>
+                    <Card key={`card`}>
+                        <Card.Header key={`card_header`}>
+                            <Row>
+                                <Col md={8}>
+                                    <Row>
+                                        <Col lg={6}><b>Title:</b>&nbsp;{hike.title}</Col>
+                                        <Col lg={6}><b>Location:</b>&nbsp;{hike.country},&nbsp;{hike.region},&nbsp;{hike.city}</Col>
+                                    </Row>
+                                </Col>
+                                <Col md={4}>
+                                    <Col className='d-flex justify-content-md-end'>
+                                        <OverlayTrigger overlay={!authObject.authUser ? <Tooltip id="tooltip-disabled">Sign up to see more info about the hike</Tooltip> : <></>}>
+                                            <Button
+                                                id={hike.id}
+                                                size='sm'
+                                                variant='success'
+                                                onClick={authObject.authUser ? (ev) => handleShowInfo(ev) : null}>
+                                                Show more info
+                                            </Button>
+                                        </OverlayTrigger>
+                                        {authObject.authUser && authObject.authUser.role.toLowerCase() === 'hiker' &&
+                                            <Button id={hike.id}
+                                                size='sm'
+                                                variant='primary'
+                                                onClick={() => {
+                                                    setHike(hike);
+                                                    setShowConfirm(true)
+                                                }}>
+                                                Start Hike
+                                            </Button>}
+                                    </Col>
+                                </Col>
+                            </Row>
+                        </Card.Header>
+                        <Card.Body className='d-flex justify-content-start' key={`card_body`}>
+                            <Col><b>Description:</b>&nbsp;<Col className='hike-desc'>{hike.description}</Col></Col>
+                        </Card.Body>
+                        <Card.Footer key={`card_footer`}>
+                            <Row md={12}>
+                                <Col md={6} lg={3} key={`hike_diff`}><b>Difficulty:</b>&nbsp;{hike.difficulty}</Col>
+                                <Col md={6} lg={3} key={`hike_len`}><b>Length:</b>&nbsp;{(parseFloat(hike.length) / 1000.).toFixed(1)}&nbsp;km</Col>
+                                <Col md={6} lg={3} key={`hike_asc`}><b>Ascent:</b>&nbsp;{parseInt(hike.ascent)}&nbsp;m</Col>
+                                <Col md={6} lg={3} key={`hike_time`}><b>Estimated Time:</b>&nbsp;{hike.expectedTime}&nbsp;min</Col>
+                            </Row>
+                        </Card.Footer>
+                    </Card>
+                    <AdditionalHikeInfoModal hike={hike} show={showInfoModal} onHide={() => setShowInfoModal(false)} />
+                    <ConfirmModal show={showConfirm} onSubmit={() => { setShowConfirm(s => !s); startHike(hike.id) }} onAbort={() => { setShowConfirm(false); }} />
+                </>
+            )}
+        </AuthenticationContext.Consumer>
+    );
+}
+
+export default HikeCard
