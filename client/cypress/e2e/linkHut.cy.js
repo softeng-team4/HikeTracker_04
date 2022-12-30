dd286e4274f7495f5b1856a9b84b7a9e410f665d
eelielisa <eelielisa@outlook.com>
2022-12-30 16:57:07 +0100

    fix: validateRole

--- client/cypress/e2e/linkHut.cy.js
+++ client/cypress/e2e/linkHut.cy.js
@@ -1,17 +1,12 @@
 describe('link huts', () => {
-    it('login', () => {
-        cy.visit('http://localhost:3000/login')
-        cy.get('.email-input').clear()
-        cy.get('.password-input').clear()
-        cy.get('.email-input').type('dqy0828@gmail.com')
-        cy.get('.password-input').type('123456789')
-        cy.contains('Login').click()
-        cy.url().should('include', '/')
-    })
 
     it('link hut', () => {
-        cy.get('.form-check-input').click()
-        cy.get('.btn-danger').click()
+        cy.login('dqy0828@gmail.com','123456789')
+        cy.get('.dropdown-toggle').click()
+        cy.get('.dropdown-menu > .modify-page').click()
+        cy.url().should('include', '/myHikeList')
+        cy.get(':nth-child(5) > .card > .card-header > :nth-child(1) > .col-md-4 > .d-flex > .btn-group > .btn-primary').click()
+        cy.url().should('include', '/modifyHike')
         cy.url().should('include', '/modifyHike')
         cy.get('#modify-tab-handler-tab-LinkHuts').click()
         cy.get('.awesome-marker-icon-blue').click()
@@ -19,37 +14,20 @@ describe('link huts', () => {
         cy.get('.btn-success').click()
         //cy.get('.modal-footer > .btn-success')
         cy.get('.btn-secondary').click()
-    })
-
-    
-    it('logout', () => {
-        cy.get('.dropdown-toggle').click()
-        cy.get('.dropdown-menu > :nth-child(3)').click()
-    })
-
-    it('login', () => {
-        cy.visit('http://localhost:3000/login')
-        cy.get('.email-input').clear()
-        cy.get('.password-input').clear()
-        cy.get('.email-input').type('aleganino@gmail.com')
-        cy.get('.password-input').type('password')
-        cy.contains('Login').click()
-        cy.wait(1000)
-        cy.url().should('include', '/')
+        cy.logout()
     })
 
     it('hike without possible huts', () => {
-        cy.visit('http://localhost:3000/')
-        cy.get('.form-check-input').click()
-        cy.get(':nth-child(4) > .card > .card-header > .row > .d-flex > .btn-group > .btn-danger').click()
+        cy.login("aleganino@gmail.com","password")
+        cy.url().should('include', '/')
+        cy.get('.dropdown-toggle').click()
+        cy.get('.dropdown-menu > .modify-page').click()
+        cy.url().should('include', '/myHikeList')
+        cy.get(':nth-child(5) > .card > .card-header > :nth-child(1) > .col-md-4 > .d-flex > .btn-group > .btn-primary').click()
         cy.url().should('include', '/modifyHike')
         cy.get('#modify-tab-handler-tab-LinkHuts').click()
         cy.contains("There are not available huts close to this hike to be linked")
-    })
-
-    it('logout', () => {
-        cy.get('.dropdown-toggle').click()
-        cy.get('.dropdown-menu > :nth-child(3)').click()
+        cy.logout()
     })
 
 })
