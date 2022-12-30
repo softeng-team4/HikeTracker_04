a5f9660a4d1b330c33cade40124dc423c84cc990
mistru97 <luca.mistruzzi@gmail.com>
2022-12-30 19:19:02 +0100

    fix: login/logout commands

--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -1,4 +1,5 @@
 import API from "../../src/API"
+const fireAuth = require('firebase/auth');
 import 'cypress-file-upload';
 // ***********************************************
 // This example commands.js shows you how to
@@ -27,17 +28,23 @@ import 'cypress-file-upload';
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('login', (email, password) => {
-        cy.visit('http://localhost:3000/login')
-        cy.get('form > :nth-child(1) > #username').clear().type(email)
-        cy.get('form > :nth-child(2) > #password').clear().type(password)
-        cy.contains('Login').click()
-        cy.url().should('include', '/')
+        // cy.visit('http://localhost:3000/login')
+        // cy.get('form > :nth-child(1) > #username').clear().type(email)
+        // cy.get('form > :nth-child(2) > #password').clear().type(password)
+        // cy.contains('Login').click()
+        // cy.url().should('include', '/')
+        const auth = fireAuth.getAuth();
+        fireAuth.signOut(auth);
+        fireAuth.signInWithEmailAndPassword(auth, email, password);
+        cy.visit('http://localhost:3000/')
 })
 
 Cypress.Commands.add('logout', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('.userDropdownButton').click()
-    cy.get('.logOutBtn').click()
+    // cy.visit('http://localhost:3000/')
+    // cy.get('.userDropdownButton').click()
+    // cy.get('.logOutBtn').click()
+    const auth = fireAuth.getAuth();
+    fireAuth.signOut(auth);
 })
 
 Cypress.Commands.add("createTestHike", (hike = {}) => {
