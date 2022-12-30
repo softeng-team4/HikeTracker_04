664506733155dbbe9964226432aa5ca9213f8648
mistru97 <luca.mistruzzi@gmail.com>
2022-12-30 17:02:24 +0100

    add: config to enable coverage

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -4,7 +4,10 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config)
+      return config
     },
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'http://localhost:3000',
+    experimentalRunAllSpecs: true
   }
 })
