57efd68067a2cc1d5b6b286396c8e22bf8f95f87
mistru97 <luca.mistruzzi@gmail.com>
2023-01-07 17:51:07 +0100

    add: e2e tests

--- client/cypress/e2e/recordPointTest.cy.js
+++ client/cypress/e2e/recordPointTest.cy.js
@@ -2,7 +2,8 @@ describe("test record point of regHike", () => {
 
     before(() => {
         cy.login('masterale1999@gmail.com', 'password')
-        cy.wait(3000)
+        cy.url().should('eq', 'http://localhost:3000/')
+        cy.wait(4000)
         cy.get('.CountrySelection').select("Italy")
         cy.get('.RegionSelection').select("Piedmont")
         cy.get('.CitySelection').select("Alba")
@@ -10,18 +11,48 @@ describe("test record point of regHike", () => {
         cy.wait(1000)
         cy.get('.btn-primary').click()
         cy.wait(1000)
-        cy.get('.btn[value="Yes"]').click()
-        cy.get('.h2').should('have.value', 'Active Hike')
+        cy.contains('.btn', 'Yes').click()
+        cy.url().should('eq', 'http://localhost:3000/active')
+        cy.wait(1000)
     })
 
     after(() => {
+        cy.get('.btn-danger').click()
+        cy.contains('.btn', 'Yes').click()
         cy.logout();
-        cy.get('btn-danger').click()
-        cy.get('.btn-success').click()
     })
 
     it("visualize map and reference points", () => {
-        cy.get('btn-primary').click()
-        cy.get('fa-info-circle').should('have.length', 4)
+        cy.get('.btn-primary').click()
+        cy.get('.fa-info-circle').should('have.length', 3)
+    })
+
+    it("add a record point", () => {
+        cy.get('.addPoint_0').click()
+        cy.wait(300)
+        cy.contains('.addPoint_0')
+        cy.contains('.addPoint_1')
+        cy.contains('.removePoint_0')
+    })
+
+    it("add a second record point", () => {
+        cy.get('.addPoint_0').click()
+        cy.wait(300)
+        cy.contains('.addPoint_0')
+        cy.contains('.removePoint_0')
+    })
+
+    it("add a third record point", () => {
+        cy.get('.addPoint_0').click()
+        cy.wait(300)
+        cy.contains('.addPoint_0').should('not.exist')
+        cy.contains('.removePoint_0')
+    })
+
+    it("remove last inserted record point", () => {
+        cy.get('.removePoint_0').click()
+        cy.wait(300)
+        cy.contains('.addPoint_0')
+        cy.contains('.removePoint_0')
     })
 })
